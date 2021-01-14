c0251b88ec378476dee6a84f8a6792ab3b1a202a
evgmolt <evg.molt@gmail.com>
2021-01-14 13:48:41 +0400

    Home work 1

---
+++ HomeWorkLesson1/Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+
+    class Program
+    {
+        static int numOfCalls;
+        static long FibRecurs(int n)
+        {
+            numOfCalls++;
+            if (n == 0) return 0;
+            if (n == 1 || n == 2) return 1;
+            return FibRecurs(n - 1) + FibRecurs(n - 2);
+        }
+
+        static long FibCircle(int n)
+        {
+            long a = 0;
+            long b = 1;
+            for (int i = 0; i < n; i++)
+            {
+                long temp = a;
+                a = b;
+                b = temp + b;
+            }
+            return a;
+        }
+
+
+        static void Main()
+        {
+            Console.Write("Введите число : ");
+            if (!Int32.TryParse(Console.ReadLine(), out int num))
+            {
+                Console.WriteLine("Ошибка ввода. Введите число.");
+                Console.ReadKey();
+                return;
+            };
+            Console.WriteLine($"Число Фибоначчи для {num} : {FibRecurs(num)}, рекурсивно");
+            Console.WriteLine($"Число рекурсивных вызовов : {numOfCalls}");
+            Console.WriteLine($"Число Фибоначчи для {num} : {FibCircle(num)}, нерекурсивно (цикл)");
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ HomeWorkLesson1/PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static string strPrimeNum = "Простое число";
+        static string strNoPrimeNum = "Непростое число";
+
+        static bool IsPrime(int number)
+        {
+            int d = 0;
+            for (int i = 2; i < number; i++)
+                if (number % i == 0)
+                    d++;
+            return d == 0;
+        }
+        static void Main()
+        {
+            int num = 1;
+            do
+            {
+                Console.Write("Введите число, 0 для выхода : ");
+                if (!Int32.TryParse(Console.ReadLine(), out num))
+                {
+                    Console.WriteLine("Ошибка ввода. Введите число.");
+                    num = 1;
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine(IsPrime(num) ? strPrimeNum : strNoPrimeNum);
+                    Console.WriteLine("Нажмите любую клавишу");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
+            while (num != 0);
+        }
+    }
+}
