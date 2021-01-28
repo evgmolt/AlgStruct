fbe1586f7de4c50c2b4b0d10af4869c9846be72f
evgmolt <evg.molt@gmail.com>
2021-01-28 12:29:50 +0400

    Home work 4

---
+++ HomeWorkLesson4/BinTree/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinTree
+{
+    class Program
+    {
+        static readonly int NumOfNodes = 6;
+        static void Main()
+        {
+            int h = Console.LargestWindowHeight;
+            int w = Console.LargestWindowWidth;
+            Console.WindowHeight = h - h / 10;
+            Console.WindowWidth = w - w / 10;
+
+            BinaryTree BTree = new BinaryTree();
+//            Random rand = new Random();
+            //            for (int i = 0; i < NumOfNodes; i++)
+            //                BTree.AddNode((int)(rand.NextDouble() * 100));
+            BTree.AddNode(70);
+            BTree.AddNode(60);
+            BTree.AddNode(170);
+            BTree.AddNode(30);
+            BTree.AddNode(140);
+            BTree.AddNode(130);
+            BTree.AddNode(50);
+            BTree.AddNode(190);
+            BTree.AddNode(150);
+            Node<int> n = BTree.SearchNode(130);
+            string s = n?.Data.ToString() ?? "null";
+            Console.WriteLine("Найдено: " + s);
+            Console.WriteLine($"Удаление " + n.ToString());
+            BTree.RemoveNode(130);
+            n = BTree.SearchNode(130);
+            s = n?.Data.ToString() ?? "null";
+            Console.WriteLine("Найдено: " + s);
+
+            Console.WriteLine($"Высота дерева : {BTree.GetTreeHeight(BTree.Head)}");
+            BTree.ShowTree(BTree.Head);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWorkLesson4/HashSetTest2/Program.cs
@@ -0,0 +1,59 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HashSetTest2
+{
+    public class BechmarkClass
+    {
+        public static int SizeOfData = 10000;
+        public static int SingleStringSize = 20;
+        public string[] StringArray;
+        public HashSet<string> hashSet;
+        public BechmarkClass()
+        {
+            StringArray = new string[SizeOfData];
+            hashSet = new HashSet<string>();
+
+            Random rand = new Random();
+            for (int i = 0; i < StringArray.Length; i++)
+            {
+                byte[] bArr = new byte[SingleStringSize];
+                rand.NextBytes(bArr);
+                string s = Encoding.UTF8.GetString(bArr);
+                StringArray[i] = s;
+                hashSet.Add(s);
+            }
+        }
+
+        [Benchmark]
+        public void SearchString()
+        {
+            string s = "bbvb";
+            for (int i = 0; i < StringArray.Length; i++)
+            {
+                if (s == StringArray[i])
+                    s = "";
+            }
+        }
+
+        [Benchmark]
+        public void SearchHash()
+        {
+            string s = "mbvbvf,faf";
+            hashSet.Contains(s);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
+        }
+
+    }
+}
